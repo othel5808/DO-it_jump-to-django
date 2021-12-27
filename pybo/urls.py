9ee931371cc699e4dcecf305f1c90f3bcfa2065b
chang-gichae <othel5808>
2021-12-28 01:21:20 +0900

    점프 투 장고 2장 완료

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.urls import path
 
 from pybo import views
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('queation/create/', views.question_create, name='question_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 
 from pybo.models import Question, Answer
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -33,8 +34,37 @@ def answer_create(request, question_id):
   answer 내용 입력
   """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(
-        content=request.POST.get('content'),
-        create_date=timezone.now(),
-    )
-    return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.create_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    # todo 나중에 `render()`와 `redirect()`의 차이점에 대해 확인 할 것
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def question_create(request):
+    """
+    question 생성
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+
+    # method == 'GET'
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    # return render(request, 'pybo/question_form.html', {'form': form})
+    return render(request, 'pybo/question_form.html', context)
